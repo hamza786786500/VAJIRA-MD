f993ccebf3262ed5014e54c8af852fbfd87cc65c
hamza786786500 <anasala120o@gmail.com>
2025-04-12 21:11:28 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,9 +6,9 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=NCkhiDIL#WHfIW4HC7S1orB-x4bEtF2hS3h1X3kjdAsDBPv-VsgU' : process.env.SESSION_ID,
-OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '' : process.env.OWNER_NUMBER,
-N_JID: process.env.N_JID=== undefined ? '‌,': process.env.N_JID,    
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=JSEVxYhY#VujZidBsCvDSdTmcXdG8XyQS2qeX4nb4Yy9ijsYsWbs' : process.env.SESSION_ID,
+OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '' : process.env.OWNER_NUMBER,923314110070
+N_JID: process.env.N_JID=== undefined ? '‌,': process.env.N_JID,120363270963036548@g.us ,   
 PREFIX: process.env.PREFIX || '.' ,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgres://vajiratech_user:oSIFl2xmSojMZ0rkzdd0g0W6msuVTpNN@dpg-cpd7fjv109ks73e5gtig-a.frankfurt-postgres.render.com/vajiratech' : process.env.POSTGRESQL_URL,   
 MAX_SIZE: 500,
